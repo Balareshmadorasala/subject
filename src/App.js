4e0dddbc7c70f1dddeddaa36f45b0a6a715ee957
Bala Reshma <reshists20@gmail.com>
2025-02-21 15:59:12 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import React from "react";
+
+import "bootstrap/dist/css/bootstrap.min.css";
+
+import TeachingSchedule from "./component/TeachingSchedule";
+
+function App() {
+  return (
+    <div className="App">
+      <TeachingSchedule />
+    </div>
+  );
+}
+
+export default App;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
